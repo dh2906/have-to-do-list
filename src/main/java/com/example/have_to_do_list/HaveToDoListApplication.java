dd6d5fb185ddc3977945df0e4ceba2bf47bbf168
Donghun <2dh2@naver.com>
2024-11-13 11:43:40 +0900

    feat. init project

---
+++ src/main/java/com/example/have_to_do_list/HaveToDoListApplication.java
@@ -0,0 +1,13 @@
+package com.example.have_to_do_list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaveToDoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaveToDoListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/have_to_do_list/HaveToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.have_to_do_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaveToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
